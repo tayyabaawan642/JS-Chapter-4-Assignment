c8c0611378250d169638d20cc117c499d1fa1bc5
tayyabaawan642 <157307322+tayyabaawan642@users.noreply.github.com>
2024-05-18 21:34:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,1 @@
+document.write("Variable names can only contain number, $ and _. For Example $my_1stVariable" + "<br>" + " Variables must begin with a letter, $ or _. For example $name, _name or name" + "<br>" + "Variable names are case sensitive" + "<br>" + "Variable names should not be JS keywords");
